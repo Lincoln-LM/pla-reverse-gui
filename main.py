4a8a82f7e95f186d3d993bfb8e1be95f0a225f41
zakski <zak.szadowski+git@gmail.com>
2024-07-06 13:09:32 +0100

    remove print statments

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ from qtpy.QtWidgets import QApplication
 
 if __name__ == "__main__":
     # Create the Qt application
-    print("Create the Qt application")
     app = QApplication(sys.argv)
 
     # Create the main window
-    print("Create the main window")
     window = MapWindow()
     app.setStyleSheet(qdarkstyle.load_stylesheet())
     window.show()
     window.setFocus()
 
     # Run the Qt event loop
-    print("Run the Qt event loop")
     sys.exit(app.exec())
