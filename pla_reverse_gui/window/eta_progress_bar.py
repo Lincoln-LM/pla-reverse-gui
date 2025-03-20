884bf0780d7fb77ea12e3aeef3bd9fd7bc54f970
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-03-19 20:37:58 -0600

    dummy eta text when no progress has been reported

--- pla_reverse_gui/window/eta_progress_bar.py
+++ pla_reverse_gui/window/eta_progress_bar.py
@@ -19,20 +19,21 @@ class ETAProgressBar(QProgressBar):
         if self.maximum() == 0:
             return
         QProgressBar.setValue(self, value)
-        if value == 0:
-            value = 1
-        if self.start_time is None:
+        if self.start_time is None or value == 0:
             self.start_time = time.time()
-        elapsed_time = time.time() - self.start_time
-        eta = (self.maximum() - value) * elapsed_time / value
-        eta_minutes, eta_seconds = divmod(eta, 60)
-        eta_hours, eta_minutes = divmod(eta_minutes, 60)
-        eta_str = (
-            (f"{eta_hours:02.00f}:" if eta_hours > 0 else "")
-            + (f"{eta_minutes:02.00f}:" if eta_minutes > 0 else "")
-            + f"{eta_seconds:05.02f}"
-            + ("s" if eta_hours == 0 and eta_minutes == 0 else "")
-        )
+        if value == 0:
+            eta_str = "..."
+        else:
+            elapsed_time = time.time() - self.start_time
+            eta = (self.maximum() - value) * elapsed_time / value
+            eta_minutes, eta_seconds = divmod(eta, 60)
+            eta_hours, eta_minutes = divmod(eta_minutes, 60)
+            eta_str = (
+                (f"{eta_hours:02.00f}:" if eta_hours > 0 else "")
+                + (f"{eta_minutes:02.00f}:" if eta_minutes > 0 else "")
+                + f"{eta_seconds:05.02f}"
+                + ("s" if eta_hours == 0 and eta_minutes == 0 else "")
+            )
         self.setFormat(f"{value/self.maximum()*100:.00f}% Estimated time: {eta_str}")
 
     def setMaximum(self, maximum: int) -> None:
